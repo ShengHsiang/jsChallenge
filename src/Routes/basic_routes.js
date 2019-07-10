c1ae36325cffa4644d71d7ff06878d9c2c956406
sheng <qzwxsa1234@gmail.com>
2019-07-10 20:56:24 +0800

    取消異步加載組件

--- src/Routes/basic_routes.js
+++ src/Routes/basic_routes.js
@@ -1,15 +1,19 @@
-import Loadable from '@loadable/component'
-import LandingPage from '../Pages/Landing/index'
+// import Loadable from '@loadable/component'
+// import LandingPage from '../Pages/Landing/index'
+import LandingPage from '../Pages/Landing/index.jsx'
+import TomatoClockPage from '../Pages/TomatoClock/index.jsx'
 
 const basic_routes = [
   {
     path: "/index", // 跳转路由
-    component: Loadable(()=>import('../Pages/Landing/index.jsx')), // 异步加载组件
+    component: LandingPage,
+    // component: Loadable(()=>import('../Pages/Landing/index.jsx')), // 异步加载组件
     title: '首頁' // 跳转页面的标题
   },
   {
     path: "/tomatoClock",
-    component: Loadable(() => import('../Pages/TomatoClock/index.jsx')),
+    component: TomatoClockPage,
+    // component: Loadable(() => import('../Pages/TomatoClock/index.jsx')),
     title: '番茄鐘'
   }
 ]
